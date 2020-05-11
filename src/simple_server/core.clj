e4cd87420bb6f34dc142491751fb323b33cf3192
62349135 <BZhang705@hotmail.com>
2020-05-11 23:29:27 +1000

    initial commit

--- src/simple_server/core.clj
+++ src/simple_server/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure.pprint]
             [ring.adapter.jetty :refer [run-jetty]]
 
-            [ring.util.response :refer [response created redirect not-found status]]
+            [ring.util.response :refer [response content-type created redirect not-found status]]
             [compojure.core :refer [GET POST ANY defroutes]]
             [compojure.coercions :refer [as-int]]
             [ring.middleware.defaults :as middleware]
@@ -12,19 +12,24 @@
 
 ;;; Finally, let us truly separate concerns between our "application code"
 ;;; and our "http code".  Our game now lives in its own namespace, and
-;;; is fully testable independent of our "presentation layer".
+;;; is fully testable independent of our "presentation layer". 
+
+(defn web-content [text]
+  (-> text
+      (response)
+      (content-type "text/plain")))
 
 (defn new-game-handler []
   (when (game/new-game!)
-    (response "OK- start guessing at /guess")))
+    (web-content "OK- start guessing at /guess")))
 
 (defn guess-handler [guess]
   (condp = (game/guess-answer guess)
-    nil       (-> (response  "You need to supply a guess with /guess?guess=N")
+    nil       (-> (web-content  "You need to supply a guess with /guess?guess=N")
                   (status 400))
-    :game-over (response  "Congratulations! You win!")
-    :too-low   (response "Too low.")
-    :too-high  (response  "Too high.")))
+    :game-over (web-content  "Congratulations! You win!")
+    :too-low   (web-content "Too low.")
+    :too-high  (web-content  "Too high.")))
 
 (defroutes game-routes
   (GET "/new-game" []                 (new-game-handler))
@@ -37,7 +42,7 @@
 
 (comment
   (handler (mock/request :get "/new-game"))
-  (handler (mock/request :get "/guess?guess=3"))
+  (handler (mock/request :get "/guess?guess=2"))
   (handler (mock/request :get "/dunno")))
 
 (defonce server
