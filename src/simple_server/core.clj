6dc4ba412634265c7a6853955d2a10024f343790
Alain Picard <Dr.Alain.Picard@gmail.com>
2020-05-13 11:52:13 +1000

    Demonstrate hiccup, routing contexts

--- src/simple_server/core.clj
+++ src/simple_server/core.clj
@@ -3,10 +3,11 @@
             [ring.adapter.jetty :refer [run-jetty]]
 
             [ring.util.response :refer [response created redirect not-found status]]
-            [compojure.core :refer [GET POST ANY defroutes]]
+            [compojure.core :refer [GET POST ANY defroutes context]]
             [compojure.coercions :refer [as-int]]
             [ring.middleware.defaults :as middleware]
 
+            [hiccup.core :refer [html]]
             [ring.mock.request :as mock]
             [simple-server.simple-game :as game]))
 
@@ -28,15 +29,51 @@
 
 (defroutes game-routes
   (GET "/new-game" []                 (new-game-handler))
-  (GET "/guess"    [guess :<< as-int] (guess-handler guess))
+  (GET "/guess"    [guess :<< as-int] (guess-handler guess)))
+
+
+(defn new-game-handler-html []
+  (when (game/new-game!)
+    (response
+     (html
+      [:body
+       [:h1 "Welcome to the guessing game!"]
+       [:p "OK- start guessing " [:a {:href "/guess.html"} "here"]]]))))
+
+(defn guess-handler-html [guess]
+  (println "They are guessing " guess)
+  (let [form [:div
+              [:h1 "Enter your guess"]
+              [:form {:method :post}
+               [:input {:type :text :name :guess}]]]
+        guess (as-int guess)]
+    (response
+     (html
+      [:body
+       (condp = (game/guess-answer guess)
+         nil        form
+         :game-over [:h2 "You won!"]
+         :too-low   [:div [:h2 "Too low."] form]
+         :too-high  [:div [:h2 "Too high."] form])]))))
+
+(defroutes html-game-routes
+  (GET "/new-game.html" [] (new-game-handler-html))
+  (GET "/guess.html"    [] (guess-handler-html nil))
+  (POST "/guess.html"   [guess] (guess-handler-html guess)))
+
+(defroutes handler
+  (context "/api" []
+    (-> game-routes
+        (middleware/wrap-defaults middleware/api-defaults)))
+  (context "" []
+    (-> html-game-routes
+        (middleware/wrap-defaults (dissoc middleware/site-defaults :security))))
   (ANY "*"         []                 (not-found "Sorry, No such URI on this server!")))
 
-(def handler
-  (-> game-routes
-      (middleware/wrap-defaults middleware/api-defaults)))
 
 (comment
   (handler (mock/request :get "/new-game"))
+  (handler (mock/request :get "/new-game-v2.html"))
   (handler (mock/request :get "/guess?guess=3"))
   (handler (mock/request :get "/dunno")))
 
